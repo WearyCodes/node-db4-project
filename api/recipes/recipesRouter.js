9e917f6bafe08f0a313c51af9cc040373c7331f9
Sam Sanchez <wearycodes@gmail.com>
2024-04-28 20:50:05 -0500

    WIP

---
+++ api/recipes/recipesRouter.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+router.use("*", (req, res, next) => {
+  res.json({ api: "up" });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = router;
