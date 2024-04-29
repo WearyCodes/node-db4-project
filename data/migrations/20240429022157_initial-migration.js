3589436994c46ac800cce0797a61554c597b90f8
Sam Sanchez <wearycodes@gmail.com>
2024-04-28 22:07:03 -0500

    ingredients table designed, recipes table designed

--- data/migrations/20240429022157_initial-migration.js
+++ data/migrations/20240429022157_initial-migration.js
@@ -1,16 +1,19 @@
 exports.up = async function (knex) {
   await knex.schema
     .createTable("recipes", (table) => {
-      table.increments("id");
+      table.increments("recipe_id");
+      table.string("recipe_name", 255).notNullable().unique();
     })
     .createTable("ingredients", (table) => {
-      table.increments("id");
+      table.increments("ingredient_id");
+      table.string("ingredient_name", 200).notNullable().unique();
+      table.string("ingredient_unit", 50);
     })
     .createTable("steps", (table) => {
-      table.increments("id");
+      table.increments("step_id");
     })
     .createTable("step_ingredients", (table) => {
-      table.increments("id");
+      table.increments("step_ingredient_id");
     });
 };
 
